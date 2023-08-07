7969f446cbbe2828cd3aa19e9dea521ff4f7f8dd
projectdasw <project.dasw@gmail.com>
2023-08-07 21:09:55 +0700

    Initial commit

---
+++ public_good_games/__init__.py
@@ -0,0 +1,76 @@
+from otree.api import *
+
+
+class C(BaseConstants):
+    NAME_IN_URL = 'public_good_games'
+    PLAYERS_PER_GROUP = 5
+    NUM_ROUNDS = 1
+    ENDOWMENT = cu(100)
+    MULTIPLIER = 2
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    total_contribution = models.CurrencyField()
+    individual_share = models.CurrencyField()
+
+
+class Player(BasePlayer):
+    contribution = models.CurrencyField(
+        min=0, max=C.ENDOWMENT, label='How many tokens do you think are in your group account?'
+    )
+    code_number = models.IntegerField(label="Code Number")
+    ncsu = models.StringField(label="NCSU Student ID number")
+    gender = models.StringField(widget=widgets.RadioSelect, choices=['Male', 'Female'], label="Gender")
+    major = models.StringField(label="Major")
+    semester = models.IntegerField(label="Semester currently in")
+    invest = models.IntegerField(
+        widget=widgets.RadioSelect, choices=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    )
+    profit_tokens = models.CurrencyField()
+
+
+# FUNCTIONS
+def set_payoffs(group: Group):
+    players = group.get_players()
+    total_invest = [p.invest for p in players]
+    group.total_contribution = sum(total_invest)
+    group.individual_share = (
+        (group.total_contribution * C.MULTIPLIER) / C.PLAYERS_PER_GROUP
+    )
+    for p in players:
+        p.profit_tokens = (20 - p.invest + group.individual_share) / 4
+        p.payoff = (p.profit_tokens / 4)
+
+
+# PAGES
+class InstructionsSheet(Page):
+    pass
+
+
+class Questionnaire(Page):
+    form_model = 'player'
+    form_fields = ['code_number', 'ncsu', 'gender', 'major', 'semester']
+
+
+class Contribute(Page):
+    form_model = 'player'
+    form_fields = ['contribution', 'invest']
+
+
+class ResultsWaitPage(WaitPage):
+    after_all_players_arrive = set_payoffs
+
+
+class Results(Page):
+    pass
+
+
+class FinalResults(Page):
+    pass
+
+
+page_sequence = [InstructionsSheet, Questionnaire, Contribute, ResultsWaitPage, Results, FinalResults]
---
+++ settings.py
@@ -0,0 +1,40 @@
+from os import environ
+
+SESSION_CONFIGS = [
+    dict(
+        display_name='Alabama Project',
+        name='alabama_project',
+        app_sequence=['public_good_games'],
+        num_demo_participants=5,
+    ),
+]
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+)
+
+PARTICIPANT_FIELDS = []
+SESSION_FIELDS = []
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'USD'
+USE_POINTS = True
+POINTS_CUSTOM_NAME = 'tokens'
+Debug = False
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+DEMO_PAGE_INTRO_HTML = """ """
+
+SECRET_KEY = '2093792563359'
